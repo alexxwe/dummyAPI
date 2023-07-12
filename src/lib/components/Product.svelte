f99d341369a781ea6f9d571c8be60158523d9ae7
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-07-12 14:15:14 +0200

    Product.svelte card

--- src/lib/components/Product.svelte
+++ src/lib/components/Product.svelte
@@ -12,21 +12,24 @@
         const stars = '🌕'.repeat(index) + extra
         return stars
     }
-
 </script>
 
 <div class="flex">
     <li class=" text-xl rounded-lg bg-zinc-400 p-4 text-center {bgColor[product.category]} {shadowColor[product.category]}">
-        <p class="text-2xl"> #{product.id} {product.title}</p>
-        <p>{product.price}€</p>
-        <p>SALE: {product.discountPercentage}%</p>
-        <div class="border border-white">
+        <p class="text-3xl"> #{product.id} {product.title.charAt(0).toUpperCase() + product.title.slice(1)}</p>
+        <p class="text-4xl">{product.price}€</p>
+
+        <div class="flex justify-between text-4xl my-4">
+            <p>SALE: {product.discountPercentage}% </p>
+            <p>STOCK: {product.stock }</p>
+
+        </div>
+            <div class="border border-white">
             <span>{rating(product.rating)}</span>
             <p>Rating: {product.rating}</p>
         </div>
-        <!-- <p>{product.stock }</p> -->
         <p>{product.brand}</p>
         <img src={product.thumbnail} alt={product.title} class="h-40 my-4 mx-auto">
-        <p class="text-black"> {product.description}</p>
+        <p class="text-black text-2xl"> {product.description}</p>
     </li>
 </div>
