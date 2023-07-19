c7cd8331cb9cd6c31a1658260c7c52776ede88ae
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2023-07-19 17:57:59 +0200

    word search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,6 @@
     import '../app.css'
     import type { ProductDto } from '$lib/types/productDto'
     import Product from '$lib/components/Product.svelte'
-    import { each } from 'svelte/internal'
     import type { userDto } from '$lib/types/userDto'
 
     /** @type {import('./$types').PageData} */
@@ -11,15 +10,15 @@
         users: Array<userDto>
     }
 
-    let search = ''
+    let categorySearch = ''
     let categories: string[] = []
 
     async function handleSearch() {
-        if (!search) {
+        if (!categorySearch) {
             window.location.reload()
             return
         }
-        const response = await fetch(`https://dummyjson.com/products/category/${encodeURIComponent(search).toLowerCase()}`)
+        const response = await fetch(`https://dummyjson.com/products/category/${encodeURIComponent(categorySearch).toLowerCase()}`)
         const searchData = await response.json()
         data.products = searchData.products
     }
@@ -31,17 +30,68 @@
     fetchCategories()
 
     let selectedUser: userDto = data.users[0]
-    let userDefault = false
 
     function handleUser(e: any) {
         const selectedIndex = e.target.selectedIndex
         selectedUser = data.users[selectedIndex - 1]
     }
-    // const uniqueTitles = [...new Set(data.products.map(product => product.title))];
-    // console.log(uniqueTitles)
+
+    let search = ''
+
+    async function wordSearch() {
+        const wordResponse = await fetch(`https://dummyjson.com/products/search?q=${encodeURIComponent(search)}`)
+        const wordData = await wordResponse.json()
+        data.products = wordData.products
+
+    }
+
+    // const uniqueProps = [...new Set(data.products.map(product => product.title))];
+    // console.log(uniqueProps)
 </script>
 
-<header class="flex items-center justify-end p-6">
+<header class="space-b flex justify-between p-6">
+    <!-- max-w-7xl = 80rem (1280px)  -::::-  max-w-screen-2xl = (1536px)-->
+    <div>
+        <input
+            type="text"
+            placeholder="Category Search"
+            class="rounded border px-1 text-black"
+            bind:value={categorySearch}
+            on:keydown={event => {
+                if (event.key === 'Enter') {
+                    handleSearch()
+                }
+            }}
+            list="categories-list"
+        />
+
+        <datalist id="categories-list">
+            {#each categories as category}
+                <option value={category} />
+            {/each}
+        </datalist>
+
+        <button class="rounded bg-zinc-500 py-1 px-2 text-white hover:bg-zinc-700" on:click={handleSearch}> Search </button><br>
+        
+        <input
+        type="text"
+        placeholder="Search"
+        class="rounded border px-1 text-black"
+        bind:value={search}
+        on:keydown={event => {
+            if (event.key === 'Enter') {
+                wordSearch()
+            }
+        }}
+        />
+
+
+        <button class="rounded bg-zinc-500 py-1 px-2 text-white hover:bg-zinc-700" on:click={wordSearch}> Search </button><br>
+            
+        </div>
+            
+    <h1 class="flex justify-center text-4xl">DummyAPI</h1>
+
     <div class="flex items-end">
         <img src={selectedUser.image} class="h-20" alt={selectedUser.firstName} />
         <p class="mr-6">{selectedUser.firstName} {selectedUser.id}</p>
@@ -54,31 +104,6 @@
     </div>
 </header>
 
-<!-- max-w-7xl = 80rem (1280px)  -::::-  max-w-screen-2xl = (1536px)-->
-<h1 class="flex justify-center text-2xl">DummyAPI</h1>
-<div class="pb-4">
-    <input
-        type="text"
-        placeholder="Category Search"
-        class="rounded border px-1 text-black"
-        bind:value={search}
-        on:keydown={event => {
-            if (event.key === 'Enter') {
-                handleSearch()
-            }
-        }}
-        list="categories-list"
-    />
-
-    <datalist id="categories-list">
-        {#each categories as category}
-            <option value={category} />
-        {/each}
-    </datalist>
-
-    <button class="rounded bg-zinc-500 py-1 px-2 text-white hover:bg-zinc-700" on:click={handleSearch}> Search </button>
-</div>
-
 <div class="mx-auto max-w-screen-2xl">
     {#if data.products.length === 0}
         <p class="p-4 text-center text-4xl font-bold">ERROR: Categoria no disponible</p>
@@ -86,12 +111,6 @@
         <ul class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
             {#each data.products as product}
                 <Product {product} />
-            {/each}
-        </ul>
-        <ul class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-            {#each data.users as user}
-                <p class="text-3xl">#{user.id} {user.firstName}{user.lastName}</p>
-                <img src={user.image} alt={user.firstName} class="my-4 mx-auto h-40" />
             {/each}
         </ul>
     {/if}
