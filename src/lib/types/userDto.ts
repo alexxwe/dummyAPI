d8bbcd8f68f11bdfe7e1591fb7dab3a99eab1a83
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-02-22 18:04:55 +0100

    Update userDto.ts

--- src/lib/types/userDto.ts
+++ src/lib/types/userDto.ts
@@ -1,66 +1,67 @@
-export interface Pokedex {
-    users: userDto[];
+export interface Users {
+    users: userDto[]
+    user: userDto
 }
 
 export interface userDto {
-    id:         number;
-    firstName:  string;
-    lastName:   string;
-    maidenName: string;
-    age:        number;
-    gender:     string;
-    email:      string;
-    phone:      string;
-    username:   string;
-    password:   string;
-    birthDate:  Date;
-    image:      string;
-    bloodGroup: string;
-    height:     number;
-    weight:     number;
-    eyeColor:   string;
-    hair:       Hair;
-    domain:     string;
-    ip:         string;
-    address:    Address;
-    macAddress: string;
-    university: string;
-    bank:       Bank;
-    company:    Company;
-    ein:        string;
-    ssn:        string;
-    userAgent:  string;
+    id: number
+    firstName: string
+    lastName: string
+    maidenName: string
+    age: number
+    gender: string
+    email: string
+    phone: string
+    username: string
+    password: string
+    birthDate: Date
+    image: string
+    bloodGroup: string
+    height: number
+    weight: number
+    eyeColor: string
+    hair: Hair
+    domain: string
+    ip: string
+    address: Address
+    macAddress: string
+    university: string
+    bank: Bank
+    company: Company
+    ein: string
+    ssn: string
+    userAgent: string
 }
 
 export interface Address {
-    address:     string;
-    city:        string;
-    coordinates: Coordinates;
-    postalCode:  string;
-    state:       string;
+    address: string
+    city: string
+    coordinates: Coordinates
+    postalCode: string
+    state: string
 }
 
 export interface Coordinates {
-    lat: number;
-    lng: number;
+    lat: number
+    lng: number
 }
 
 export interface Bank {
-    cardExpire: string;
-    cardNumber: string;
-    cardType:   string;
-    currency:   string;
-    iban:       string;
+    cardExpire: string
+    cardNumber: string
+    cardType: string
+    currency: string
+    iban: string
 }
 
 export interface Company {
-    address:    Address;
-    department: string;
-    name:       string;
-    title:      string;
+    address: Address
+    department: string
+    name: string
+    title: string
 }
 
 export interface Hair {
-    color: string;
-    type:  string;
+    color: string
+    type: string
 }
