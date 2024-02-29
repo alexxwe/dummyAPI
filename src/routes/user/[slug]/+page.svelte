83414ae2ef3bd0d8d12f6814d12c9a077792d440
Alex Paz <91157929+alexxwe@users.noreply.github.com>
2024-02-29 18:18:23 +0100

    all data squares

--- src/routes/user/[slug]/+page.svelte
+++ src/routes/user/[slug]/+page.svelte
@@ -20,21 +20,21 @@
     <div class="mx-auto {isFront ? '' : 'flipped'}" on:click={toggleCard}>
         <div class="flip-container {userColor[data.user.id]} {isFront ? '' : 'flipped'} ">
             <article class="front m-2 grid grid-cols-3">
-                <img class="h-12 mx-auto" src={data.user.image} alt={data.user.image} title={data.user.image} />
+                <img class="h-12 mx-auto col-span-3" src={data.user.image} alt={data.user.image} title={data.user.image} />
                 <p>User ID: {data.user.id}</p>
                 <p>First Name: {data.user.firstName}</p>
                 <p>Last Name: {data.user.lastName}</p>
                 <p>Maiden Name: {data.user.maidenName}</p>
                 <p>Age: {data.user.age}</p>
-                <p>Gender: {data.user.gender}</p>
+                <p>Gender: {data.user.gender === 'male' ? 'M' : 'F'}</p>
                 <p>Email: {data.user.email}</p>
-                <p><i class="bi bi-telephone-fill"></i>Phone: {data.user.phone}</p>
+                <p><i class="bi bi-telephone-fill"></i> Phone: {data.user.phone}</p>
                 <p>Username: {data.user.username}</p>
-                <p>Birth Date: {data.user.birthDate}</p>
-                <p>Blood Group: {data.user.bloodGroup}</p>
+                <p><i class="bi bi-cake-fill"></i> Birth Date: {data.user.birthDate}</p>
+                <p><i class="bi bi-droplet-fill"></i> Blood Group: {data.user.bloodGroup}</p>
                 <p>Height: {data.user.height}</p>
                 <p>Weight: {data.user.weight}</p>
-                <p>Eye Color: {data.user.eyeColor}</p>
+                <p><i class="bi bi-eye-fill"> </i> Eye Color: {data.user.eyeColor}</p>
                 <p>Hair Color: {data.user.hair.color}</p>
                 <p>Hair Type: {data.user.hair.type}</p>
                 <p>Domain: {data.user.domain}</p>
@@ -98,3 +98,48 @@
         transform: rotateY(180deg);
     }
 </style>
+
+<div>
+    <!-- Accessing user data -->
+    <p>
+        Image:
+        <img class="h-12" src={data.user.image} alt={data.user.image} title={data.user.image} />
+    </p>
+    <p>User ID: {data.user.id}</p>
+    <p>First Name: {data.user.firstName}</p>
+    <p>Last Name: {data.user.lastName}</p>
+    <p>Maiden Name: {data.user.maidenName}</p>
+    <p>Age: {data.user.age}</p>
+    <p>Gender: {data.user.gender}</p>
+    <p>Email: {data.user.email}</p>
+    <p>Phone: {data.user.phone}</p>
+    <p>Username: {data.user.username}</p>
+    <p>Password: {data.user.password}</p>
+    <p>Birth Date: {data.user.birthDate}</p>
+    <p>Blood Group: {data.user.bloodGroup}</p>
+    <p>Height: {data.user.height}</p>
+    <p>Weight: {data.user.weight}</p>
+    <p>Eye Color: {data.user.eyeColor}</p>
+    <p>Hair Color: {data.user.hair.color}</p>
+    <p>Hair Type: {data.user.hair.type}</p>
+    <p>Domain: {data.user.domain}</p>
+    <p>IP Address: {data.user.ip}</p>
+    <p>MAC Address: {data.user.macAddress}</p>
+    <p>University: {data.user.university}</p>
+    <p>Bank Card Expire: {data.user.bank.cardExpire}</p>
+    <p>Bank Card Number: {data.user.bank.cardNumber}</p>
+    <p>Bank Card Type: {data.user.bank.cardType}</p>
+    <p>Bank Currency: {data.user.bank.currency}</p>
+    <p>Bank IBAN: {data.user.bank.iban}</p>
+    <p>Company Department: {data.user.company.department}</p>
+    <p>Company Name: {data.user.company.name}</p>
+    <p>Company Title: {data.user.company.title}</p>
+    <p>User Agent: {data.user.userAgent}</p>
+
+    <!-- Accessing user address information -->
+    <p>Address: {data.user.address.address}</p>
+    <p>City: {data.user.address.city}</p>
+    <p>Postal Code: {data.user.address.postalCode}</p>
+    <p>State: {data.user.address.state}</p>
+    <p>Coordinates: {data.user.address.coordinates.lat}, {data.user.address.coordinates.lng}</p>
+</div>
